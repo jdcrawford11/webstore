f4303cd40c401c30430feb63934bcd6db5cc1b2c
Jaleesa <crawfordjaleesa@gmail.com>
2016-07-21 22:14:18 -0500

    revised database variables

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database' => getenv('DB_NAME'),
-			'username' => getenv('DB_USERNAME'),
-			'password' => getenv('DB_PASSWORD'),
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
